d77b5f9b56f5cf36492c2b6a885310bccb486d70
mache12 <40327066+mache12@users.noreply.github.com>
2019-06-19 01:11:43 -0500

    Add files via upload

---
+++ Version_final/luteach_web/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+application = Flask(__name__)
+
+application.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+
+application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(application)
+
+bcrypt = Bcrypt(application)
+
+login_manager = LoginManager(application)
+
+login_manager.login_view = 'login'
+
+login_manager.login_message_category = 'info'
+
+from luteach_web import routes
---
+++ Version_final/luteach_web/routes.py
@@ -0,0 +1,111 @@
+import secrets
+import os
+from PIL import Image
+from flask import render_template, url_for, flash, redirect, request
+from luteach_web.forms import FormDeRegistros, LoginForm, UpdateAccountForm, LearnForm, TeachForm
+from luteach_web.models import User
+from luteach_web import application, db, bcrypt
+from flask_login import login_user, current_user, logout_user, login_required
+
+@application.route("/")
+def home():
+    return render_template('home.html')
+
+@application.route("/services")
+def services():
+    return render_template('services.html')
+
+@application.route("/about")
+def about():
+    return render_template('about.html')
+
+@application.route("/contact")
+def contact():
+    return render_template('contact.html')
+
+@application.route("/register", methods=['GET','POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = FormDeRegistros()
+    if form.validate_on_submit():
+       hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+       user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+       db.session.add(user)
+       db.session.commit()
+       flash('your account has been created! You are now able to log in','success')
+       return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@application.route("/login", methods=['GET','POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+       user = User.query.filter_by(email=form.email.data).first()
+       if user and bcrypt.check_password_hash(user.password, form.password.data):
+          login_user(user, remember=form.remember)
+          next_page = request.args.get('next')
+          return redirect(next_page) if next_page else redirect(url_for('home'))
+       else:
+          flash('Login error, pls check unername and password','danger')
+    return render_template('login.html', title='login', form=form)
+
+@application.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(application.root_path, 'static/profile_pics', picture_fn)
+
+    output_size = (125,125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+    return picture_fn
+
+
+@application.route("/account", methods=['GET','POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+
+        current_user.username = form.username.data
+        current_user.username = form.email.data
+        db.session.commit()
+        flash('Your account has been created', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+
+@application.route("/learn", methods=['GET', 'POST'])
+@login_required
+def new_learn():
+    form = LearnForm()
+    if form.validate_on_submit():
+        flash('Gracias por aprender con nosotros!', 'success')
+        return redirect(url_for('home'))
+    return render_template('learn.html', title='learn with us', form=form)
+
+@application.route("/teach", methods=['GET', 'POST'])
+@login_required
+def new_teach():
+    form = TeachForm()
+    if form.validate_on_submit():
+        flash('Gracias por confiar en nosotros!', 'success')
+        return redirect(url_for('home'))
+    return render_template('teach.html', title='learn with us', form=form)
